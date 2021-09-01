0516934e3774c0a9e91a0f338945e19a84946018
Ryan How <ryanhow1999@yahoo.ca>
2021-09-01 17:17:34 -0400

    init comit

---
+++ CFLP.java
@@ -0,0 +1,22 @@
+public class CFLP {
+	
+	public static void main(String[] args) {
+		/*
+		 * Arguments
+		 * 1 - file with matrix of demand of product k for customer r
+		 * 2 - file with production plant capacities
+		 * 3 - file with minimum activity level for facilities
+		 * 4 - file with maximum activity level for facilities 
+		 * 5 - file with fixed cost for facilities
+		 * 6 - file with marginal cost for facilities
+		 * 7 - file with unit transportation cost from plant to facility
+		 * 8 - file with unit transportation cost from facility to customer
+		 * 9 - file with distances from plant to facility
+		 * 10 - file with distances from facility to customer
+		 * 11 - desired number of facilities to be open
+		 * 12 - "single" or "divisible" 
+		 * 
+		 */
+	}
+	
+}
